cac87b0644c77f16aa757d1de53214e7569ea99b
jeremyfelt <jeremy.felt@gmail.com>
2024-09-03 20:57:36 -0700

    Update unlock.js to prep for a detection script

--- src/components/block-editor/unlock.js
+++ src/components/block-editor/unlock.js
@@ -3,8 +3,11 @@
  */
 import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/private-apis';
 
+const latest = 'I acknowledge private features are not for use in themes or plugins and doing so will break in the next version of WordPress.';
+const old    = 'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.';
+
 export const { unlock } =
     __dangerousOptInToUnstableAPIsOnlyForCoreModules(
-        'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.',
+        latest,
         '@wordpress/edit-post'
     );
